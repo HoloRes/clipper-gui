daa3356adb96a3e6e53f8d0329b61f0dd243879e
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2021-03-26 11:10:07 +0100

    Trying to add a clip from current time in video feature

--- renderer/pages/home.js
+++ renderer/pages/home.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Head from 'next/head';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import ReactPlayer from 'react-player';
@@ -22,11 +22,39 @@ const useStyles = makeStyles(theme =>
 export default function Home() {
   const classes = useStyles({});
   const [clips, setClips] = useState([]);
+  const [clipsHtml, setClipsHtml] = useState([]);
+  const [currentClip, setCurrentClip] = useState([]);
   const [videoUrl, setVideoUrl] = useState('');
+  const playerRef = useRef();
 
-  const urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/gi;
+  function secondParser(hms) {
+    const a = hms.split(':');
+    return (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+  }
+
+  function timestampParser(seconds) {
+    const date = new Date(1970,0,1);
+    date.setSeconds(seconds);
+    return date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+  }
 
   function handleClip() {}
+
+  function clipTimestamp() {
+    const clip = [...currentClip];
+    clip.push(playerRef.getCurrentTime());
+    setCurrentClip(clip);
+    if(currentClip[0]) {
+      const newClips = [...clips];
+      newClips.push(clip);
+      setClips(newClips);
+      setCurrentClip([]);
+    }
+  }
+
+  useEffect(() => {
+    console.log(clips);
+  }, [clips])
 
   return (
     <>
@@ -37,9 +65,10 @@ export default function Home() {
         <Typography variant="h4" gutterBottom>
           Clipper
         </Typography>
-        {videoUrl && urlRegex.test(videoUrl) && (
+        {videoUrl && ReactPlayer.canPlay(videoUrl) && (
           <ReactPlayer
             url={videoUrl}
+            ref={playerRef}
             light
             style={{ marginLeft: 'auto', marginRight: 'auto' }}
           />
@@ -47,12 +76,22 @@ export default function Home() {
         <Input
           placeholder="Video URL"
           value={videoUrl}
-          error={!urlRegex.test(videoUrl)}
+          error={!ReactPlayer.canPlay(videoUrl)}
           onChange={event => setVideoUrl(event.target.value)}
         />
-        <br />
+        <br/>
+        <br/>
+        <Button
+            variant="contained"
+            color="secondary"
+            onClick={clipTimestamp}
+            disabled={!ReactPlayer.canPlay(videoUrl) || !playerRef?.getCurrentTime || playerRef?.getCurrentTime() === null}
+        >
+          { currentClip.length > 0 ? "End clip" : "Start clip" }
+        </Button>
+        <br/>
+        <br/>
         <Typography>Clips:</Typography>
-        <br />
         <br />
         <Grid item xs={12}>
           <Grid container justify="center" alignItems="center">
